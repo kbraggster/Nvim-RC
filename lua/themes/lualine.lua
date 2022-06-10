c6df425500367b9f9674f175bedd64706d3e7d44
Kyler Bragg <braggky@gmail.com>
2022-06-09 21:32:57 -0400

    Fixed LSP keymappings

--- lua/themes/lualine.lua
+++ lua/themes/lualine.lua
@@ -14,7 +14,7 @@ require('lualine').setup {
     lualine_c = {'filename'},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
-    lualine_z = {'%c:%l/%L'}
+    lualine_z = {'%l/%L:%c'}
   },
   inactive_sections = {
     lualine_a = {},
