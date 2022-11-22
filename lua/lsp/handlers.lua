014de00c16ce5587d88953f1b1c9a04296d0b4e2
Kyler Bragg <braggky@gmail.com>
2022-11-21 21:12:18 -0500

    Updated deprecated function in lsp

--- lua/lsp/handlers.lua
+++ lua/lsp/handlers.lua
@@ -72,7 +72,7 @@ if not status_ok then
 	return
 end
 
-M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+M.capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 M.capabilities.offsetEncoding = { "utf-16" }
 
 return M
