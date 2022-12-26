5423b46b3e3ab57366104958e568d77d2f039650
Kyler Bragg <braggky@gmail.com>
2022-12-25 23:11:10 -0500

    Removed 'commit' arg from LSP plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -50,11 +50,11 @@ return packer.startup(function(use)
   use "ellisonleao/gruvbox.nvim"
 
   -- LSP
-  use { "neovim/nvim-lspconfig", commit = "f11fdff7e8b5b415e5ef1837bdcdd37ea6764dda" } -- enable LSP
-  use { "williamboman/mason.nvim", commit = "c2002d7a6b5a72ba02388548cfaf420b864fbc12"} -- simple to use language server installer
-  use { "williamboman/mason-lspconfig.nvim", commit = "0051870dd728f4988110a1b2d47f4a4510213e31" }
-  use { "jose-elias-alvarez/null-ls.nvim", commit = "c0c19f32b614b3921e17886c541c13a72748d450" } -- for formatters and linters
-  --use { "RRethy/vim-illuminate", commit = "a2e8476af3f3e993bb0d6477438aad3096512e42" }
+  use { "neovim/nvim-lspconfig" } -- enable LSP
+  use { "williamboman/mason.nvim" } -- simple to use language server installer
+  use { "williamboman/mason-lspconfig.nvim" }
+  use { "jose-elias-alvarez/null-ls.nvim" } -- for formatters and linters
+  use { "RRethy/vim-illuminate" }
 
   -- CMP Plugins
   use "hrsh7th/nvim-cmp"
