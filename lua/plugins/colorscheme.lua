90b047d6830b5dd0029c1fcf85a5574beff22b55
Kyler Bragg <braggky@gmail.com>
2024-04-23 22:17:25 -0400

    Updated colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -12,11 +12,31 @@ return {
   lazy = false,
   name = "catppuccin",
 
+  -- add papercolor
+  { "NLKNguyen/papercolor-theme" },
+  lazy = false,
+  name = "papercolor",
+
+  -- add onedark
+  {
+    "navarasu/onedark.nvim",
+    opts = {
+      style = "dark"
+    }
+  },
+
+  -- add kanagawa
+  {
+    "rebelot/kanagawa.nvim",
+    opts = {
+    }
+  },
+
   -- Configure LazyVim to load colorscheme
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "kanagawa",
     },
   },
 }
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,26 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      clangd = {
+        cmd = {
+          "clangd",
+          "--background-index",
+          "--clang-tidy",
+          "--header-insertion=iwyu",
+          "--completion-style=detailed",
+          "--function-arg-placeholders",
+          "--fallback-style=llvm",
+          "--header-insertion=never"
+        },
+      },
+    },
+    setup = {
+      clangd = function(_, opts)
+        local clangd_ext_opts = require("lazyvim.util").opts("clangd_extensions.nvim")
+        require("clangd_extensions").setup(vim.tbl_deep_extend("force", clangd_ext_opts or {}, { server = opts }))
+        return false
+      end,
+    },
+  },
+}
